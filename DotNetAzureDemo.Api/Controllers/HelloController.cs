79833779a700739ade061b086bdd816eeb13f69a
mihirmaru21 <mihirmaru21@gmail.com>
2026-04-20 11:40:50 +0530

    Initian .net Core API project with Test Project

---
+++ DotNetAzureDemo.Api/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetAzureDemo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is Working from Azure 🚀");
+        }
+    }
+}
---
+++ DotNetAzureDemo.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DotNetAzureDemo.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
